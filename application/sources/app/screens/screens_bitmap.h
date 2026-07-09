24bb6a8b6624d854860605e5bd16baf0d015757b
KimThanhNguyen1409 <nkimthanh47@gmail.com>
2026-07-09 17:36:55 +0700

    Adding BITMAP for OBJECTS

--- application/sources/app/screens/screens_bitmap.h
+++ application/sources/app/screens/screens_bitmap.h
@@ -3,7 +3,27 @@
 
 #include "view_render.h"
 
-// scr_welcome
-extern const unsigned char PROGMEM bitmap_dolphin[];
+/*--------------------------------In game Objects--------------------------------*/
+extern const unsigned char PROGMEM main_sub[];
+extern const unsigned char PROGMEM bomb[];
+extern const unsigned char PROGMEM heart[];
+extern const unsigned char PROGMEM boom_I[];
+extern const unsigned char PROGMEM boom_II[];
+extern const unsigned char PROGMEM boom_III[];
+extern const unsigned char PROGMEM boom_IV[];
+extern const unsigned char PROGMEM boom_V[];
+extern const unsigned char PROGMEM boom_VI[];
+extern const unsigned char PROGMEM coin[];
+extern const unsigned char PROGMEM spike_triple[];
+extern const unsigned char PROGMEM spike_single[];
+/*--------------------------------Start menu Objects--------------------------------*/
+extern const unsigned char PROGMEM seaweed_I[];
+extern const unsigned char PROGMEM seaweed_II[];
+extern const unsigned char PROGMEM seaweed_III[];
+extern const unsigned char PROGMEM start_sub[];
+/*--------------------------------Over game Objects--------------------------------*/
+extern const unsigned char PROGMEM skull[];
+extern const unsigned char PROGMEM fish[];
+extern const unsigned char PROGMEM ground[];
 
 #endif //__SCREENS_BITMAP_H__
