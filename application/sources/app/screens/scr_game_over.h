4bb7842c97e1deb40cd49432159ecfc76837c073
KimThanhNguyen-hcmus <nkimthanh47@gmail.com>
2026-07-30 11:59:56 +0700

    Update game  over's UI

--- application/sources/app/screens/scr_game_over.h
+++ application/sources/app/screens/scr_game_over.h
@@ -15,11 +15,20 @@
 #include "task_display.h"
 #include "view_render.h"
 
+#include "eeprom.h"
+#include "app_eeprom.h"
+
 #include "screens.h"
 #include "screens_bitmap.h"
 
 #include "scr_game_in_the_depth.h"
 #include "scr_game_menu.h"
+#include "scr_game_ranking.h"
+
+#define HOME_BITMAP_AXIS_X  (24)
+#define HOME_BITMAP_AXIS_Y  (24)
+
+
 extern view_dynamic_t dyn_view_item_game_over;
 extern view_screen_t scr_game_over;
 extern void scr_game_over_handle(ak_msg_t *msg);
