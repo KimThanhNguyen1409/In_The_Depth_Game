11eca8a2418704f11f4f6abff83a6e20992f8269
KimThanhNguyen-hcmus <nkimthanh47@gmail.com>
2026-07-30 11:58:49 +0700

    Update game menu'UI

--- application/sources/app/screens/scr_welcome.cpp
+++ application/sources/app/screens/scr_welcome.cpp
@@ -1,24 +1,17 @@
 #include "scr_welcome.h"
 
-#define SEAGRASS_BITMAP_AXIS_X (20)
-#define SEAGRASS_BITMAP_AXIS_Y (40)
-#define SEAGRASS_NUMBER (7)
-
-typedef struct{
-	uint8_t x;
-	uint8_t y;
-	uint8_t action_image;
-} itd_game_seagrass_t;
-
-static itd_game_seagrass_t seagrasses[SEAGRASS_NUMBER];
-
+static uint8_t seagrass_action_image = 1;
+static int8_t fishes_x[3] = {-10, -30, -20};                                                     
+static uint8_t fishes_y[3] = {25, 35, 42};                                                                          
+static uint8_t fishes_speed[3] = {2, 1, 3}; 
 static void view_scr_welcome();
 
 view_dynamic_t dyn_view_welcome = {
 	{
 		.item_type = ITEM_TYPE_DYNAMIC,
 	},
-	view_scr_welcome};
+	view_scr_welcome
+};
 
 view_screen_t scr_welcome = {
 	&dyn_view_welcome,
@@ -28,55 +21,61 @@ view_screen_t scr_welcome = {
 	.focus_item = 0,
 };
 void itd_game_seagrass_display(){
-	uint8_t i = 0;
-	for (i = 0; i < SEAGRASS_NUMBER; i++)
-    {
-
         const unsigned char *frame = seagrass_I;
         uint8_t w = SEAGRASS_BITMAP_AXIS_X;
         uint8_t h = SEAGRASS_BITMAP_AXIS_Y;
-        if (seagrasses[i].action_image == 2)
+        if (seagrass_action_image == 2)
         {
             frame = seagrass_II;
         }
-        else if (seagrasses[i].action_image == 3)
+        else if (seagrass_action_image == 3)
         {
             frame = seagrass_III;
         }
-        else if (seagrasses[i].action_image == 4)
+        else if (seagrass_action_image == 4)
         {
             frame = seagrass_IV;
         }
-        else if (seagrasses[i].action_image == 5)
+        else if (seagrass_action_image == 5)
         {
             frame = seagrass_V;
         }
-        else if (seagrasses[i].action_image == 6)
+        else if (seagrass_action_image == 6)
         {
             frame = seagrass_VI;
         }
-		else if (seagrasses[i].action_image == 7)
+		else if (seagrass_action_image == 7)
 		{
 			frame = seagrass_VII;
-			i = 0;
 		}
-        view_render.drawBitmap(50, 50, frame, w, h, WHITE);
-		seagrasses->action_image++;
-		
-    }
+        view_render.drawBitmap(10, 46, frame, w, h, WHITE);
+		view_render.drawBitmap(54, 46, frame, w, h, WHITE);
+		view_render.drawBitmap(84, 46, frame, w, h, WHITE);
+
 }
 void view_scr_welcome()
 {
+	static uint32_t wel_frame = 0;
+	wel_frame++;
 	view_render.clear();
-	view_render.drawFastHLine(0, 40, 128, WHITE);
-	view_render.drawBitmap(10, 45, stone, 20, 20, WHITE);
-	view_render.drawBitmap(100, 40, stone, 20, 20, WHITE);
-	view_render.drawBitmap(50, 38, crab, 32 ,32, WHITE);
-	view_render.setTextSize(1);
-    view_render.setCursor(24, 4);
-    view_render.print("IN THE DEPTH");
-	view_render.setCursor(24, 8);
-	
+	for(uint8_t i = 0; i < 4; i++){
+		uint8_t bub_x = 15 + i * 30;
+		uint8_t bub_y = 64 - ((wel_frame * i + 2 * 15) % 64);
+		view_render.drawCircle(bub_x, bub_y, 1 + (i % 2), WHITE);
+	}
+	for(int i = 0; i < 3; i++) {                                                                                                       
+        view_render.drawBitmap(fishes_x[i], fishes_y[i], fish, FISH_BITMAP_AXIS_X, FISH_BITMAP_AXIS_Y, WHITE);
+    }
+	view_render.drawBitmap(34, 2, title, TITLE_BITMAP_AXIS_X, TITLE_BITMAP_AXIS_Y, WHITE);
+	view_render.drawBitmap(0, 58, seabottom, SEABOTTOM_BITMAP_AXIS_X, SEABOTTOM_BITMAP_AXIS_Y, WHITE);
+	itd_game_seagrass_display();                                                                                     
+	view_render.drawBitmap(30, 48, stone, 20, 20, WHITE);                                                                               
+    view_render.drawBitmap(102, 48, stone, 20, 20, WHITE);
+	view_render.setTextColor(WHITE);
+	if ((wel_frame / 4) % 2 == 0) {
+        view_render.setCursor(34, 24);
+        view_render.print("PRESS MODE");
+    }
 }
 
 void scr_welcome_handle(ak_msg_t *msg)
@@ -87,11 +86,28 @@ void scr_welcome_handle(ak_msg_t *msg)
 	{
 		APP_DBG_SIG("SCREEN_ENTRY\n");
 		// BUZZER_PlaySound(BUZZER_SOUND_WELCOME);
+		itd_game_scores_read(&scores);
+		itd_game_setting_read(&settings);
 		timer_set(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK_INTERVAL, TIMER_PERIODIC);
-		timer_set(AC_TASK_DISPLAY_ID, AC_DISPLAY_SHOW_IDLE, AC_DISPLAY_IDLE_INTERVAL, TIMER_ONE_SHOT);
+		timer_set(AC_TASK_DISPLAY_ID, AC_DISPLAY_ANIMATION_TICK, 100, TIMER_PERIODIC);
 	}
 	break;
-
+	case AC_DISPLAY_ANIMATION_TICK:
+	{
+		APP_DBG_SIG("AC_DISPLAY_ANIAMTION_TICK");
+		seagrass_action_image++;
+		if(seagrass_action_image >= 7)
+			seagrass_action_image = 1;
+		for(int i = 0; i < 3; i++) {                                                                                                   
+            fishes_x[i] += fishes_speed[i];                                                                                                                                              
+            if (fishes_x[i] > 140) {                                                                                                   
+                fishes_x[i] = -10 + (rand() % -30);                                                                                                              
+                fishes_y[i] = 22 + (rand() % 24);                                                                                      
+                fishes_speed[i] = 1 + (rand() % 3);                                                                  
+            }                                                                                                                          
+        }  
+	}
+	break;
 	case AC_DISPLAY_WELCOME_TEXT_ANIM_TICK:
 	{
 		APP_DBG_SIG("AC_DISPLAY_WELCOME_TEXT_ANIM_TICK\n");
@@ -104,7 +120,6 @@ void scr_welcome_handle(ak_msg_t *msg)
 	{
 		APP_DBG_SIG("AC_DISPLAY_BUTON_MODE_PRESSED\n");
 		timer_remove_attr(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK);
-		// SCREEN_TRAN(scr_game_in_the_depth_handle, &scr_game_in_the_depth);
 		SCREEN_TRAN(scr_game_menu_handle, &scr_game_menu);
 	}
 	break;
--- application/sources/app/screens/scr_welcome.h
+++ application/sources/app/screens/scr_welcome.h
@@ -4,4 +4,23 @@
 #include "screens.h"
 #include "scr_game_in_the_depth.h"
 #include "scr_game_menu.h"
+
+#include "eeprom.h"
+#include "app_eeprom.h"
+
+#define SEAGRASS_BITMAP_AXIS_X (16)
+#define SEAGRASS_BITMAP_AXIS_Y (16)
+
+#define PARTICLE_SPAWN_AXIS_Y_MAX (45)
+#define PARTICLE_SPAWN_AXIS_Y_MIN (30)
+
+#define TITLE_BITMAP_AXIS_X  (64)
+#define TITLE_BITMAP_AXIS_Y (20)
+
+#define FISH_BITMAP_AXIS_X (12)
+#define FISH_BITMAP_AXIS_Y (8)
+
+#define CRAB_BITMAP_AXIS_X  (32)
+#define CRAB_BITMAP_AXIS_Y  (32)
+
 #endif //__SCR_WELCOME_H__
