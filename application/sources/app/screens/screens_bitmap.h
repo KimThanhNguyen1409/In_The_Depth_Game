40af4785f19cc9ec05f60da71dbd0261f28535e6
KimThanhNguyen-hcmus <nkimthanh47@gmail.com>
2026-07-30 12:01:24 +0700

    Adding new models for objects

--- application/sources/app/screens/screens_bitmap.h
+++ application/sources/app/screens/screens_bitmap.h
@@ -17,13 +17,30 @@ extern const unsigned char PROGMEM boom_VI[];
 extern const unsigned char PROGMEM coin[];
 extern const unsigned char PROGMEM barrier[];
 extern const unsigned char PROGMEM gift[];
-extern const unsigned char PROGMEM spike_triple[];
-extern const unsigned char PROGMEM spike_single[];
+extern const unsigned char PROGMEM spike_short[];
+extern const unsigned char PROGMEM spike_tall[];
 /*--------------------------------Start menu Objects--------------------------------*/
-extern const unsigned char PROGMEM crab[];
+extern const unsigned char PROGMEM dive_letter[];
+extern const unsigned char PROGMEM setting_letter[];
+extern const unsigned char PROGMEM rank_letter[];
+extern const unsigned char PROGMEM mode_letter[];
+extern const unsigned char PROGMEM speed_letter[];
+extern const unsigned char PROGMEM sound_letter[];
+extern const unsigned char PROGMEM exit_letter[];
+extern const unsigned char PROGMEM on_letter[];
+extern const unsigned char PROGMEM off_letter[];
+extern const unsigned char PROGMEM sink_letter[];
+extern const unsigned char PROGMEM score_letter[];
+extern const unsigned char PROGMEM time_letter[];
+extern const unsigned char PROGMEM skull[];
+extern const unsigned char PROGMEM fish[];
+extern const unsigned char PROGMEM chain[];
+extern const unsigned char PROGMEM title[];
+extern const unsigned char PROGMEM seabottom[];
 extern const unsigned char PROGMEM stone[];
 extern const unsigned char PROGMEM setting[];
 extern const unsigned char PROGMEM exit_icon[];
+extern const unsigned char PROGMEM setting_sub[];
 extern const unsigned char PROGMEM seagrass_I[];
 extern const unsigned char PROGMEM seagrass_II[];
 extern const unsigned char PROGMEM seagrass_III[];
@@ -32,10 +49,11 @@ extern const unsigned char PROGMEM seagrass_V[];
 extern const unsigned char PROGMEM seagrass_VI[];
 extern const unsigned char PROGMEM seagrass_VII[];
 /*--------------------------------Over game Objects--------------------------------*/
-extern const unsigned char PROGMEM grass[];
-extern const unsigned char PROGMEM fish[];
-extern const unsigned char PROGMEM ground[];
+extern const unsigned char PROGMEM sub_sinking[];
 extern const unsigned char PROGMEM home[];
 extern const unsigned char PROGMEM rank[];
 extern const unsigned char PROGMEM retry[];
+extern const unsigned char PROGMEM gameover_sub [];
+/*--------------------------------Rank game Objects--------------------------------*/
+
 #endif //__SCREENS_BITMAP_H__
